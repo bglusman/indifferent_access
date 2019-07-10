43371e8527fe85e67e5bc9f977fad2313a9c45fd
Brian Glusman <brian@stellaservice.com>
2019-07-10 16:11:01 -0400

    One alternative, don't retain the strings at all

--- lib/indifferent_access.ex
+++ lib/indifferent_access.ex
@@ -1,22 +1,25 @@
 defmodule IndifferentAccess do
   @moduledoc """
-  Recursively adds atom keys in params maps if the strings have existing atoms
+  Recursively replaces string keys with atom keys in params maps
+  if the strings have existing atoms, and the atom is not already a key
   """
 
   def indifferentize(map) when is_map(map) do
     if Map.get(map, :__struct__) do
       map
     else
+      map_keys = Map.keys(map)
+
       Enum.reduce(map, %{}, fn
         {key, value}, accum when is_binary(key) ->
           existing_atom = atoms_map()[key]
           indifferent_value = indifferentize(value)
 
-          if existing_atom,
+          if existing_atom && existing_atom not in map_keys,
             do:
               accum
               |> Map.put_new(existing_atom, indifferent_value)
-              |> Map.put(key, indifferent_value),
+              |> Map.delete(key),
             else: Map.put(accum, key, indifferent_value)
 
         {key, value}, accum ->
