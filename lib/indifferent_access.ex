b7961ce4428fb050f78be05ff1800a30c2042ed4
Brian Glusman <brian@stellaservice.com>
2019-07-10 20:28:58 -0400

    As long as we have config….

--- lib/indifferent_access.ex
+++ lib/indifferent_access.ex
@@ -5,37 +5,51 @@ defmodule IndifferentAccess do
 
   def indifferentize(params, opts \\ []) when is_map(params) do
     case opts[:as] do
-      :map -> indifferentize_map(params)
+      :map -> indifferentize_map(params, opts)
       _default -> IndifferentAccess.IndifferentParams.new(params)
     end
   end
 
-  def indifferentize_map(map) when is_map(map) do
+  def indifferentize_map(map, opts) when is_map(map) do
     if Map.get(map, :__struct__) do
       map
     else
+      map_keys = Map.keys(map)
+
       Enum.reduce(map, %{}, fn
         {key, value}, accum when is_binary(key) ->
           existing_atom = atoms_map()[key]
-          indifferent_value = indifferentize_map(value)
+          indifferent_value = indifferentize_map(value, opts)
 
-          if existing_atom,
-            do:
-              accum
-              |> Map.put_new(existing_atom, indifferent_value)
-              |> Map.put(key, indifferent_value),
-            else: Map.put(accum, key, indifferent_value)
+          case opts[:strategy] do
+            :augment ->
+              if existing_atom,
+                do:
+                  accum
+                  |> Map.put_new(existing_atom, indifferent_value)
+                  |> Map.put(key, indifferent_value),
+                else: Map.put(accum, key, indifferent_value)
+
+            _default ->
+              if existing_atom && existing_atom not in map_keys,
+                do:
+                  accum
+                  |> Map.put_new(existing_atom, indifferent_value)
+                  |> Map.delete(key),
+                else: Map.put(accum, key, indifferent_value)
+          end
 
         {key, value}, accum ->
-          indifferent_value = indifferentize_map(value)
+          indifferent_value = indifferentize_map(value, opts)
           Map.put(accum, key, indifferent_value)
       end)
     end
   end
 
-  def indifferentize_map(list) when is_list(list), do: Enum.map(list, &indifferentize_map/1)
+  def indifferentize_map(list, opts) when is_list(list),
+    do: Enum.map(list, &indifferentize_map(&1, opts))
 
-  def indifferentize_map(other), do: other
+  def indifferentize_map(other, _opts), do: other
 
   def initialize_atoms_map() do
     atoms_count = :erlang.system_info(:atom_count)
